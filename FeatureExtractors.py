804b9a3bac66df328115cfa91f8f12cd398aa620
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-12-30 10:49:52 +0000

    update Data.py e FeatureExtractors.py

--- FeatureExtractors.py
+++ FeatureExtractors.py
@@ -2,16 +2,6 @@ from Data import *
 from Utils import *
 import cv2
 
-#TODO: make sure that the names of the feature extractors are self descriptive
-'''
-General template:
-	Feature extractor receives the image as a numpy array
-	extracts relevant classification features from image
-	returns a tuple containing two numpy arrays:
-		the first including the feature names
-		the seconf including the features corresponding to those names
-'''
-
 class Feature_Extractors:
 
 	def moments_feature_extractor(self, image):
@@ -24,7 +14,6 @@ class Feature_Extractors:
 			feature_names.append(label)
 			feature_moments.append(moments[label])		
 		
-
 		return (array(feature_names), array(feature_moments))
 
 	def solidity_feature_extractor(self, image):
@@ -89,28 +78,6 @@ class Feature_Extractors:
 
 		return (feature_names, features)
 	
-	def corner_ratio_feature_extractor(self, image):
-		(feature_names1, features1) = self.corner_count_feature_extractor(image)
-		(feature_names2, features2) = self.length_width_ratio_feature_extractor(image)
-
-		return (array(list(feature_names1)+list(feature_names2)), array(list(features1)+list(features2)))
-	
-	def all_areas_feature_extractor(self, image):
-		(feature_names1, features1) = self.perimeter_area_ratio_feature_extractor(image)
-		(feature_names2, features2) = self.ratio_of_areas_feature_extractor(image)
-
-		return (array(list(feature_names1)+list(feature_names2)), array(list(features1)+list(features2)))
-	
-	def all_four_feature_extractor(self, image):
-		(feature_names1, features1) = self.corner_count_feature_extractor(image)
-		(feature_names2, features2) = self.length_width_ratio_feature_extractor(image)
-		(feature_names3, features3) = self.perimeter_area_ratio_feature_extractor(image)
-		(feature_names4, features4) = self.ratio_of_areas_feature_extractor(image)
-
-		features = array(list(features1)+list(features2)+list(features3)+list(features4))
-		feature_names = array(list(feature_names1)+list(feature_names2)+list(feature_names4)+list(feature_names4))
-		
-		return (feature_names, features)
 	
 	def all_five_feature_extractor(self, image):
 		(feature_names1, features1) = self.corner_count_feature_extractor(image)
