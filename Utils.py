74f40cea0ddbd3296e9112057f87e73b9b8997eb
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-12-31 03:49:56 +0000

    update extracção de caracteristicas

--- Utils.py
+++ Utils.py
@@ -41,45 +41,6 @@ label_to_number = {'Populus_Nigra': 69, 'Acer_platanoides':222, 'Sorbus aucupari
  					'Prunus_Avium': 43, 'Ilex_Aquifolium': 61, 'Castanea_Sativa': 44, 'Acer_Circinatum' : 12, \
 					'Acer_Platanoids': 24, 'Acer_Palmatum': 17, 'Liquidambar_Styraciflua': 52, 'Quercus_Vulcanica':122}
 
-#---------------------------------------Math Tools---------------------------------------
-#Notice: Since we started using numpy arrays instead of dictionaries to represent vectors
-#		 some of these functions are to simple to even be considered. However since they
-#		 are already done they will remain on the code.
-
-
-#precondition: The 2 vectors have the same dimensions
-#This function calculates the dot product of vectors v1 and v2. The same index on v1 and
-#v2 represents the same dimension
-#v1 is a vector represented as numpy array.
-#v2 is a vector represented as numpy array.
-#returns a scalar = the value of the dot product
-def dot_product(v1, v2):
-	dot_product_scalar = 1.0*sum(v1*v2)
-	return dot_product_scalar
-	
-#precondition: The 2 vectors have the same dimensions
-#This function calculates the sum of vectors v1 and v2
-#v1 is a vector represented as a numpy array.
-#v2 is a vector represented as a numpy array.
-#returns a vector which is the sum of v1 and v2
-def sum_vectors(v1, v2):
-	sum_vector = v1 + v2
-	return sum_vector
-
-#This function multiplies a vector times a constant
-#v is a vector
-#c is a constant
-#returns a new vector = c*v
-def scalar_multiplication(v, c):
-	scalar_mult_vector = c*v
-	return scalar_mult_vector		
-
-#This function finds the norm of a vector
-#v is a vector
-#returns the value of the norm of v
-def norm(v):
-	norm = sqrt(sum(v**2))
-	return norm
 
 
 #-------------------------------------Image Processing Tools-----------------------------
